05dad209c49e1f91b43436d15f0125aada5be43a
salmanmuddhoo <salmanmuddhoo@gmail.com>
2025-09-02 05:48:12 +0200

    Publish application

--- dist/assets/Dashboard-CprNlL8m.js
+++ dist/assets/Dashboard-CprNlL8m.js
@@ -1,4 +1,4 @@
-import{c as t,u,a as b,U as d,F as m,h as r,j as e,d as i,q as l}from"./index-CvbeNbco.js";import{u as o}from"./useQuery-yPB1E0Lv.js";/**
+import{c as t,u,a as b,U as d,F as m,h as r,j as e,d as i,q as l}from"./index-9pW-t-sw.js";import{u as o}from"./useQuery-Bi1nLucc.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- dist/assets/square-pen-BVqPWt2h.js
+++ dist/assets/square-pen-BVqPWt2h.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-CvbeNbco.js";/**
+import{c as a}from"./index-9pW-t-sw.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
