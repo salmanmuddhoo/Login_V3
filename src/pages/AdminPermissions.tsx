40630ca01d97c875b6e23b867b7e0a8c2f477e9a
salmanmuddhoo <salmanmuddhoo@gmail.com>
2025-09-03 08:43:04 +0400

    Updated AdminPermissions.tsx

--- src/pages/AdminPermissions.tsx
+++ src/pages/AdminPermissions.tsx
@@ -94,7 +94,7 @@ export function AdminPermissions() {
   }, {} as Record<string, Permission[]>)
 
   return (
-    <div className="space-y-6 pt 24">
+    <div className="space-y-6 pt-24">
       <div className="flex items-center justify-between">
         <div>
           <h1 className="text-2xl font-bold text-gray-900 flex items-center">
