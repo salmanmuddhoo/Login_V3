269a531817ae114f950869c4f1202492d8b8fa1a
salmanmuddhoo <salmanmuddhoo@gmail.com>
2025-09-03 08:46:20 +0400

    Publish application

--- dist/assets/square-pen-B1HLY_JD.js
+++ dist/assets/square-pen-B1HLY_JD.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-YG46-OtH.js";/**
+import{c as a}from"./index-DJD5zu_P.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
