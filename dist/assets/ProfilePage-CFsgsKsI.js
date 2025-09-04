b37595a52f94fcaa7693d2746e923496e165053b
salmanmuddhoo <salmanmuddhoo@gmail.com>
2025-09-04 08:21:17 +0400

    Publish application

--- dist/assets/ProfilePage-CFsgsKsI.js
+++ dist/assets/ProfilePage-CFsgsKsI.js
@@ -1,4 +1,4 @@
-import{c as w,u as b,r as a,j as e,C as N,A as y,S as j,J as v,M as S}from"./index-U1mVGTt9.js";import{S as P}from"./square-pen-oB21HRei.js";/**
+import{c as w,u as b,r as a,j as e,C as N,A as y,S as j,J as v,M as S}from"./index-BawRFzMk.js";import{S as P}from"./square-pen-CpuRqtm4.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
