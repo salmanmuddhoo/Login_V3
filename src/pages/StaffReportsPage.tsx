1e19c4881d9669dc1880d62b3ec6e2b214911c1b
salmanmuddhoo <salmanmuddhoo@gmail.com>
2025-09-06 12:04:30 +0200

    Updated StaffReportsPage.tsx

--- src/pages/StaffReportsPage.tsx
+++ src/pages/StaffReportsPage.tsx
@@ -219,7 +219,7 @@ export function StaffReportsPage() {
   }
 
   return (
-    <div className="space-y-6">
+    <div className="space-y-6 pt-24">
       <div className="flex items-center justify-between">
         <div>
           <h1 className="text-2xl font-bold text-gray-900 flex items-center">
