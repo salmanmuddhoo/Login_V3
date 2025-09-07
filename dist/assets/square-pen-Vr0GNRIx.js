2337f1a2f7b28a1346f63c6becf8b5495deb1252
salmanmuddhoo <salmanmuddhoo@gmail.com>
2025-09-07 12:00:34 +0200

    Publish application

--- dist/assets/square-pen-Vr0GNRIx.js
+++ dist/assets/square-pen-Vr0GNRIx.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-Bc8MROaC.js";/**
+import{c as a}from"./index-Ms87pTA4.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
