5ce7fbd111fe6ee08d87d545b43cbf8387785cc5
salmanmuddhoo <salmanmuddhoo@gmail.com>
2025-09-07 12:43:24 +0200

    Publish application

--- dist/assets/square-pen-DCrlGmAT.js
+++ dist/assets/square-pen-DCrlGmAT.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-D9PiPGrP.js";/**
+import{c as a}from"./index-C7z6iZ_L.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
