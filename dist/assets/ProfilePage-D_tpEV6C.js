c38803898575d74b2f102b6bbde3849ae4c751cf
salmanmuddhoo <salmanmuddhoo@gmail.com>
2025-09-07 11:49:34 +0200

    Publish application

--- dist/assets/ProfilePage-D_tpEV6C.js
+++ dist/assets/ProfilePage-D_tpEV6C.js
@@ -1,4 +1,4 @@
-import{c as w,u as b,r as a,j as e,C as N,A as y,S as j,J as v,M as S}from"./index-CE5YnCEM.js";import{S as P}from"./square-pen-CfLrBCl7.js";/**
+import{c as w,u as b,r as a,j as e,C as N,A as y,S as j,J as v,M as S}from"./index-Bc8MROaC.js";import{S as P}from"./square-pen-4zePeNyo.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- dist/assets/square-pen-4zePeNyo.js
+++ dist/assets/square-pen-4zePeNyo.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-CE5YnCEM.js";/**
+import{c as a}from"./index-Bc8MROaC.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
