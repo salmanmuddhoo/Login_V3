e369d5cd0faa2ab3e90004ec4a4f8bc3995feb09
salmanmuddhoo <salmanmuddhoo@gmail.com>
2025-09-08 08:07:42 +0400

    Publish application

--- dist/assets/square-pen-DPSPBrPr.js
+++ dist/assets/square-pen-DPSPBrPr.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-CQc4t7rQ.js";/**
+import{c as a}from"./index-DM9Q78Et.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
