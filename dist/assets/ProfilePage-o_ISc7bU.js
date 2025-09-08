fab8572cb91db98b6815ad430c883b147509de5b
salmanmuddhoo <salmanmuddhoo@gmail.com>
2025-09-08 07:52:27 +0400

    Publish application

--- dist/assets/ProfilePage-o_ISc7bU.js
+++ dist/assets/ProfilePage-o_ISc7bU.js
@@ -1,4 +1,4 @@
-import{c as w,u as b,r as a,j as e,C as N,A as y,S as j,J as v,M as S}from"./index-BG1-QImG.js";import{S as P}from"./square-pen-IC8efSti.js";/**
+import{c as w,u as b,r as a,j as e,C as N,A as y,S as j,J as v,M as S}from"./index-BNAE1sae.js";import{S as P}from"./square-pen-CyoMywIo.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
